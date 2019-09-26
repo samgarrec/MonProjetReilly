04b2ad2d4af4a6c181806e3b2d55b92810d1947d
sam <samgarre@gmail.com>
2019-09-26 19:31:43 +0200

    Fichier e=rempli html

---
+++ admin/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Projet Reilly Admonistration</title>
+</head>
+<body>
+<h1>Accès controllé</h1>
+<p>Veuillez vous identifier ci dessous </p>
+
+<form action="" method="post">
+
+    <input type="text" placeholder="Nom d'utilisateur ">
+    <input type="password" placeholder="Votre Mdp">
+    <input type="submit">
+
+</form>
+
+</body>
+</html>
